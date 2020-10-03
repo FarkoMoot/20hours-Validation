b4df930642ec6614f280f7f90bc8952cb63ed723
Gustavo Patrick Alves <farko_moot@hotmail.com>
2020-10-03 00:27:10 -0300

    day eight, 6hours

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import 'models/item.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -18,9 +20,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  //metodo Constructor
+  HomePage() {
+    items = [];
+    items.add(Item(title: 'title1', done: false));
+    items.add(Item(title: 'title2', done: false));
+    items.add(Item(title: 'title3', done: true));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("20Housr App"),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {},
+          );
+        },
+      ),
+    );
   }
 }
