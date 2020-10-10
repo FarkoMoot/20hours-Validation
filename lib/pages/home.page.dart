d3c194c6934e7110c2afaf12193c8cd1efc68267
Gustavo Patrick Alves <farko_moot@hotmail.com>
2020-10-10 18:25:01 -0300

    day thirteen, 10hours

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -3,30 +3,89 @@ import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:hours20/widgets/logo.widget.dart';
 import 'package:hours20/widgets/submit-form.dart';
+import 'package:hours20/widgets/sucess.widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color _color = Colors.deepPurple;
   var _gasCtrl = new MoneyMaskedTextController();
+
   var _alcCtrl = new MoneyMaskedTextController();
+
+  var _busy = false;
+
+  var _completed = false;
+
+  var _resultText = "Compensa Y";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          // SucessPag(
-          //   result: "Compensa X",
-          //   reset: () {},
-          // ),
-          SubmitForm(
-            gasCtrl: _gasCtrl,
-            alcCtrl: _alcCtrl,
-            busy: false,
-            submitFunc: () {},
-          ),
-        ],
+      body: AnimatedContainer(
+        duration: Duration(milliseconds: 1200),
+        color: _color,
+        child: ListView(
+          children: <Widget>[
+            Logo(),
+            _completed
+                ? SucessPag(
+                    result: _resultText,
+                    reset: reset,
+                  )
+                : SubmitForm(
+                    gasCtrl: _gasCtrl,
+                    alcCtrl: _alcCtrl,
+                    busy: _busy,
+                    submitFunc: calculate,
+                  ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future calculate() {
+    double alc =
+        double.parse(_alcCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+    double gas =
+        double.parse(_gasCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+
+    double res = alc / gas;
+
+    setState(() {
+      _color = Colors.red;
+      _completed = false;
+      _busy = true;
+    });
+
+    return new Future.delayed(
+        const Duration(seconds: 1),
+        () => {
+              setState(() {
+                if (res >= 0.7) {
+                  _resultText = "Compensa usar Gasosa!";
+                } else {
+                  _resultText = "Compensa usar etanois!";
+                }
+
+                _busy = false;
+                _completed = true;
+              }),
+            });
+  }
+
+  reset() {
+    setState(() {
+      _color = Colors.deepPurple;
+      _gasCtrl = new MoneyMaskedTextController();
+      _alcCtrl = new MoneyMaskedTextController();
+      _completed = false;
+      _busy = false;
+    });
   }
 }
